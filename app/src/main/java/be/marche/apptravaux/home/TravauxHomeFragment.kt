c36acbb15346cdf58632193233affea457c69e41
jfsenechal <jfsenechal@gmail.com>
2020-03-17 21:01:39 +0100

    homepage

---
+++ app/src/main/java/be/marche/apptravaux/home/TravauxHomeFragment.kt
@@ -0,0 +1,31 @@
+package be.marche.apptravaux.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import be.marche.apptravaux.R
+import be.marche.apptravaux.databinding.FragmentTravauxHomeBinding
+
+class TravauxHomeFragment : Fragment() {
+    private var _binding: FragmentTravauxHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentTravauxHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        binding.btnGoAvaloir.setOnClickListener {
+            findNavController().navigate(R.id.action_travauxHomeFragment_to_homeFragment)
+        }
+    }
+}
