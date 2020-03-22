665e3760055eb587779ab454a3022772adad3047
jfsenechal <jfsenechal@gmail.com>
2020-03-22 11:35:07 +0100

    ajout de coil, retirer glide

---
+++ app/src/main/java/be/marche/apptravaux/user/entity/User.kt
@@ -0,0 +1,18 @@
+package be.marche.apptravaux.user.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity()
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val nom: String,
+    val prenom: String,
+    val email: String,
+    val token: String
+) {
+    override fun toString(): String {
+        return email
+    }
+}
