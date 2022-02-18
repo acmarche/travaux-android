a4ec73d2087e3e81fe569de3c85fa05bf09eb6f0
jfsenechal <jfsenechal@gmail.com>
2022-02-18 19:16:40 +0100

    add log

--- app/src/main/java/be/marche/apptravaux/screens/stock/StockHomeScreen.kt
+++ app/src/main/java/be/marche/apptravaux/screens/stock/StockHomeScreen.kt
@@ -1,9 +1,11 @@
 package be.marche.apptravaux.screens.stock
 
+import android.util.Log
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun StockHomeScreen() {
+    Log.d("ZEZE", "Stock jome")
     Text("Home scree")
 }
